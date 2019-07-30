dd341b882a0d8f89bc8f4eced675703c414ea4bb
zhiwj <314581947@qq.com>
2019-07-30 14:55:26 +0800

    ch02

--- src/jvmgo/ch02/classpath/Classpath.go
+++ src/jvmgo/ch02/classpath/Classpath.go
@@ -1,5 +1,10 @@
 package classpath
 
+import (
+	"path/filepath"
+	"os"
+)
+
 type Classpath struct {
 	bootClasspath Entry
 	extClasspath  Entry
@@ -7,11 +12,62 @@ type Classpath struct {
 }
 
 func Parse(jreOption string, cpOption string) *Classpath {
-	return nil
+	cp := new(Classpath)
+	cp.parseBootAndExtClasspath(jreOption)
+	cp.parseUserClasspath(cpOption)
+	return cp
 }
+
+func (self *Classpath) parseBootAndExtClasspath(jreOption string) {
+	jreDir := getJreDir(jreOption)
+	// jre/lib/*
+	jreLibPath := filepath.Join(jreDir, "lib", "*")
+	self.bootClasspath = newWildcardEntry(jreLibPath)
+	jreExtPath := filepath.Join(jreDir, "lib", "ext", "*")
+	self.extClasspath = newWildcardEntry(jreExtPath)
+}
+
+func getJreDir(jreOption string) string {
+	if jreOption != "" && exists(jreOption) {
+		return jreOption
+	}
+	if exists("./jre") {
+		return "./jre"
+	}
+	if jh := os.Getenv("JAVA_HOME"); jh != "" {
+		return filepath.Join(jh, "jre")
+	}
+	panic("Can not find jre folder!")
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func (self *Classpath) parseUserClasspath(cpOption string) {
+	if cpOption == "" {
+		cpOption = "."
+	}
+	self.userClasspath = newEntry(cpOption)
+}
+
 func (self *Classpath) ReadClass(className string) ([]byte, Entry, error) {
-	return nil, nil, nil
+	className = className + ".class"
+	if data, entry, err := self.bootClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	if data, entry, err := self.extClasspath.readClass(className); err == nil {
+		return data, entry, err
+	}
+	return self.userClasspath.readClass(className)
 }
+
 func (self *Classpath) String() (string) {
+	return self.userClasspath.String()
 	return ""
 }
--- src/jvmgo/ch02/main.go
+++ src/jvmgo/ch02/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"jvmgo/ch02/classpath"
+	"strings"
 )
 
 func main() {
-	cmd := parseCmd()
+	//cmd := parseCmd()
+	cmd := new(Cmd)
+	cmd.class = "java.lang.Object"
 	if cmd.versionFlag {
 		fmt.Println("version 0.0.1")
 	}else if cmd.helpFlag || cmd.class == "" {
@@ -14,6 +18,16 @@ func main() {
 		startJVM(cmd)
 	}
 }
+
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class:%s args:%v\n", cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n",
+		cp, cmd.class, cmd.args)
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+	fmt.Printf("class data:%v\n", classData)
 }
