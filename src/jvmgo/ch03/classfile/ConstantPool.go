14df066dbd76b3d0932075dba2ac469d4ed92c0c
zhiwj <314581947@qq.com>
2019-08-02 18:05:30 +0800

    ch03

---
+++ src/jvmgo/ch03/classfile/ConstantPool.go
@@ -0,0 +1,9 @@
+package classfile
+
+type ConstantPool []ConstantInfo
+
+func readConstantPool(reader *ClassReader) ConstantPool {...}
+func (self ConstantPool) getConstantInfo(index uint16) ConstantInfo {...}
+func (self ConstantPool) getNameAndType(index uint16) (string, string) {...}
+func (self ConstantPool) getClassName(index uint16) string {...}
+func (self ConstantPool) getUtf8(index uint16) string {...}
